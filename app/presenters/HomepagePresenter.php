01d05a1a12d7151fe5878c3fda7f6653560f8ca0
pavel.suk <pavel.suk@aira.cz>
2017-09-08 19:13:58 +0200

    Uploading starting project

---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use App\Model\ArticleManager;
+
+
+class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+    /** @var ArticleManager */
+    private $articleManager;
+
+    public function __construct(ArticleManager $articleManager)
+    {
+        $this->articleManager = $articleManager;
+    }
+
+    public function renderDefault()
+    {
+        $this->template->posts = $this->articleManager->getPublicArticles()->limit(5);
+    }
+
+}
+
